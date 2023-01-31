ce0502f0b6ca7dead7d82b98fd4fe51bcacb9b73
Viachaslau Kalevich <cto@noirix.com>
2023-01-31 18:22:39 +0100

    Initial Commit

---
+++ src/main/java/com/noirix/Main.java
@@ -0,0 +1,18 @@
+package com.noirix;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Main {
+
+    //Ctrl+Alt+O - import optimizing
+    //Ctrl+Alt+L - formatting
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println(RandomStringUtils.random(10, true, true));
+        System.out.println(RandomStringUtils.random(10, true, true));
+        System.out.println(RandomStringUtils.random(10, true, true));
+
+    }
+}
