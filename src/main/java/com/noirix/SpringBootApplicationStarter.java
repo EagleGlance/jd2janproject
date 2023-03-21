e5db7560b0f3e7db8fc0cb072f729b98de662bae
Viachaslau Kalevich <cto@noirix.com>
2023-03-21 18:35:01 +0100

    Spring Boot: First Step

---
+++ src/main/java/com/noirix/SpringBootApplicationStarter.java
@@ -0,0 +1,20 @@
+package com.noirix;
+
+import com.noirix.configuration.ApplicationConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication(scanBasePackages = "com.noirix")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableWebMvc
+@Import(
+        ApplicationConfig.class
+)
+public class SpringBootApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationStarter.class, args);
+    }
+}
--- src/main/java/com/noirix/configuration/ApplicationConfig.java
+++ src/main/java/com/noirix/configuration/ApplicationConfig.java
@@ -4,26 +4,16 @@ import com.noirix.util.RandomValuesGenerator;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
 
 @Configuration
-@EnableAspectJAutoProxy(proxyTargetClass = true)
-@EnableWebMvc
 public class ApplicationConfig {
 
     @Bean
-    //@Scope("singleton")
-    //@Scope("prototype")
-    public RandomValuesGenerator getRandomGenerator(
-//            @Autowired @Qualifier("userRepositoryImpl") UserRepository userRepository) { - example of autowiring
-//            UserRepository userRepository) { - example of autowiring
-    ) {
-
+    public RandomValuesGenerator getRandomGenerator() {
         return new RandomValuesGenerator();
     }
 
